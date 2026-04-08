210d1f693187f132062a3fe2ae041b77f7cab3e0
alvinwikblad <161339723+alvinwikblad@users.noreply.github.com>
2026-04-08 09:11:23 +0200

    Add files via upload

--- lab1/Task1/Models/Lstm_model.py
+++ lab1/Task1/Models/Lstm_model.py
@@ -3,12 +3,16 @@ import torch.nn as nn
 
 class LSTM(nn.Module):
     def __init__(self, vocab_size):
+        
         super().__init__()
         print("LSTM constucted")
         # define layers here later
-        self.embedding = nn.Embedding(vocab_size, 25)
-        self.lstm=nn.LSTM(25,64,batch_first=True)
-        self.relu1=nn.ReLU()
+        self.embedding = nn.Embedding(vocab_size, 30,padding_idx=0)
+        self.lstm=nn.LSTM(30,64,batch_first=True,num_layers=5,dropout=0.3)
+        
+        self.dropout = nn.Dropout(0.3)
+        #self.relu1=nn.ReLU()
+        
         self.fc1=nn.Linear(64,2)
 
     def forward(self, x):
@@ -16,12 +20,18 @@ class LSTM(nn.Module):
         x = self.embedding(x)
         # → (batch_size, seq_len, embedding_dim)
 
-        out, _ = self.lstm(x)
+        #out, _ = self.lstm(x)
         # → (batch_size, seq_len, hidden_size)
 
-        out = out[:, -1, :]
+        #out = out[:, -1, :]
+
+
+        _, (h_n, _) = self.lstm(x)
+        out = h_n[-1]
+        
         # → (batch_size, hidden_size)
-        out = self.relu1(out)
+        out=self.dropout(out)
+       # out = self.relu1(out)
         out = self.fc1(out)
         # → (batch_size, 2)
 
